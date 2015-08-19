e3571df70b9c339015a6151fa9f5bdcd2b5e27d3
Tara Newberry <newberrytara30@gmail.com>
2015-08-19 08:23:07 -0500

    now with styling

--- app.js
+++ app.js
@@ -1,32 +1,18 @@
-var randomNum = function (min, max) {
-    return Math.floor(Math.random() * (1 + max - min) + min);
-}
 
 var app = angular.module('myApp', []);
 
 app.controller('myController', function($scope, TwitterHandle){
     $scope.handles = TwitterHandle;
-    $scope.clickMe = function(event) {
-         event.preventDefault();
-    }
 });
 
 app.factory('AdjectiveService', function(){
 
     var adjArray = ['super', 'red', 'boisterous', 'gargantuan', 'funny', 'sweet', 'sassy', 'rainy', 'salty', 'monsoonal'];
-
-    //var randomAdj = adjArray[randomNum(0, adjArray.length - 1)];
-    //
-    //var adjectives = {adjective: randomAdj};
     return adjArray;
 });
 
 app.factory('NounService', function(){
     var nounArray = ['cat', 'dog', 'chicken', 'goat', 'bear', 'horse', 'cow', 'tiger', 'sheep', 'lion'];
-
-    //var randomNoun = nounArray[randomNum(0, nounArray.length - 1)];
-    //
-    //var nouns = {noun: randomNoun};
     return nounArray;
 });
 
